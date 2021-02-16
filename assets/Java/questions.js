baf8e7f4bb74f283ca5fa0d8dd86461e12c2e82e
Zayad <zayad.khan.1998@gmail.com>
2021-02-16 18:38:51 -0500

    adding script folder containing questions

---
+++ assets/Java/questions.js
@@ -0,0 +1,12 @@
+// This file will hold the questions for the quiz.
+// The questions will be a multiple choice in which the user will be given 4 choices with only 1 correct answer.
+// Mainly done this way in order to de-clutter the script file.
+
+var questions = [
+    {
+        question: "Inside which HTML element is the JavaScript inserted?",
+        choices:["h1","script","javascript","body"],
+        answer:2
+
+    },
+]
